333f03310b02135d07215263ad7969473d9c6741
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2023-03-13 17:45:11 +0100

    Added dbContext

---
+++ projekt_njnt/Models/dbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace projekt_njnt.Models
+{
+    public class dbContext:DbContext
+    {
+        public dbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //public DbSet<Set> Name { get; set; }
+    }
+}
--- projekt_njnt/Program.cs
+++ projekt_njnt/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using projekt_njnt.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
-
+//Db
+builder.Services.AddDbContext<dbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
