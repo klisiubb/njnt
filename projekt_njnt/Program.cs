173abbb2396ccfb1672b8cd793168f40c5c1d7e9
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2023-03-13 18:08:33 +0100

    Made Mudblazor work & fixed Program.cs

--- projekt_njnt/Program.cs
+++ projekt_njnt/Program.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 using projekt_njnt.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-var app = builder.Build();
+//Mudblazor
+builder.Services.AddMudServices();
+
 //Db
 builder.Services.AddDbContext<dbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
